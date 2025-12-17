11d0df1c1544f2932ae200144e8fec8d9a903d5a
Manoj <chausali.manoj9@gmail.com>
2025-12-17 11:39:16 +0530

    second deploy using pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     
     <div className="container">
-      <p>Deployed via CodePipeline 🚀</p>
+      <p>Deployed via CodePipeline 🚀🚀🚀🚀</p>
       <h1 className="header-name">Manoj Chausali</h1>
       <p className="header-tagline">Cybersecurity | AWS | DevOps</p>
 
